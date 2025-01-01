3a0edbaf09ed0260d213f37a016805751e56bac6
Ummer Farooq <farooq.tfora@gmail.com>
2025-01-01 15:08:46 +0530

    Add order consumer to process events on startup

--- order_service/main.py
+++ order_service/main.py
@@ -1,4 +1,8 @@
+import threading
+
 from fastapi import FastAPI
+from messaging import order_queue
+from messaging.services import process_order_events
 from order.routes import router as order_router
 
 app = FastAPI(
@@ -6,3 +10,14 @@ app = FastAPI(
 )
 
 app.include_router(order_router)
+
+
+def start_order_consumer():
+    order_queue.consume(process_order_events)
+    order_queue.start_consuming()
+
+
+@app.on_event("startup")
+async def startup_event():
+    consumer_thread = threading.Thread(target=start_order_consumer, daemon=True)
+    consumer_thread.start()
