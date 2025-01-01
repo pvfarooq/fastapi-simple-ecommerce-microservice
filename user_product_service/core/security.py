b0f84220a9fd961529effeaff7266112fb3c3e60
Ummer Farooq <farooq.tfora@gmail.com>
2025-01-02 01:23:01 +0530

    Implement password hashing and JWT authentication in user product service

--- user_product_service/core/security.py
+++ user_product_service/core/security.py
@@ -1,12 +1,31 @@
-from passlib.context import CryptContext
+import jwt
+from core.db import session_local
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from users import crud
 
-# Password hashing context
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from .config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=401, detail="Invalid authentication credentials"
+            )
+        token_data = {"user_id": user_id}
+        with session_local() as session:
+            user = crud.get_user_by_id(token_data["user_id"], session)
+            if user is None:
+                raise HTTPException(status_code=404, detail="Invalid user")
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=401, detail="Invalid authentication credentials"
+        )
+    return token_data
