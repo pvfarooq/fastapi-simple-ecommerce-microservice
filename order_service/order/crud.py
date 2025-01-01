ad7832f753fb322fbb88f4da5ab0c5124d244ae8
Ummer Farooq <farooq.tfora@gmail.com>
2025-01-01 15:08:13 +0530

    Implement order cancellation and stock validation in order service

--- order_service/order/crud.py
+++ order_service/order/crud.py
@@ -1,4 +1,3 @@
-from messaging.publisher import StockDeductionPublisher
 from sqlalchemy.orm import Session
 
 from .models import Order
@@ -6,19 +5,26 @@ from .schemas import OrderRequest
 from .utils import generate_order_id
 
 
-def create_order(current_user, session: Session, order: OrderRequest):
+def create_order(current_user, session: Session, order: OrderRequest) -> Order:
     new_order = Order(
         user_id=current_user,
         product_id=order.product_id,
         quantity=order.quantity,
+        status="confirmed",
     )
     new_order.order_id = generate_order_id(session)
     session.add(new_order)
     session.commit()
-
-    stock_publisher = StockDeductionPublisher()
-    stock_publisher.publish_stock_deduction(
-        product_id=order.product_id, quantity=order.quantity
-    )
-
     return new_order
+
+
+def cancel_order(order_id: int, session: Session) -> Order:
+    order = get_order_by_id(order_id, session)
+    order.status = "cancelled"
+    session.commit()
+    return order
+
+
+def get_order_by_id(order_id: int, session: Session) -> Order:
+    query = session.query(Order).filter(Order.id == order_id)
+    return query.first()
--- order_service/order/routes.py
+++ order_service/order/routes.py
@@ -1,5 +1,6 @@
 from core.db import get_session
 from fastapi import APIRouter, Depends
+from messaging.services import publish_product_stock_reduction, validate_product_stock
 from order import crud
 
 from .schemas import OrderRequest
@@ -10,5 +11,20 @@ router = APIRouter(prefix="/orders", tags=["Orders"])
 @router.post("/create")
 def create_order(request: OrderRequest, session=Depends(get_session)):
     current_user = 1
+    is_stock_available = validate_product_stock(request.dict())
+    if not is_stock_available["in_stock"]:
+        return {"message": is_stock_available["message"]}
+
     order = crud.create_order(current_user, session, request)
-    return {"message": "Order placed successfully", "order_id": order.order_id}
+    publish_product_stock_reduction(order)
+    return {"message": "Order placed successfully"}
+
+
+@router.get("/cancel/{order_id}")
+def cancel_order(order_id: int, session=Depends(get_session)):
+    order = crud.get_order_by_id(order_id, session)
+    return {"order": order}
+    # if order:
+    #     crud.cancel_order(order, session)
+    #     return {"message": "Order cancelled successfully"}
+    # return {"message": "Order not found"}
