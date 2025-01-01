e8464e0df9a01e21e51e968c29fea23abe737b86
Ummer Farooq <farooq.tfora@gmail.com>
2025-01-02 01:22:49 +0530

    Add access token expiration and secret key to configuration

--- user_product_service/core/config.py
+++ user_product_service/core/config.py
@@ -11,6 +11,7 @@ class Config:
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
     ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_SECONDS = 60 * 60 * 24 * 7  # 1 week
 
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
