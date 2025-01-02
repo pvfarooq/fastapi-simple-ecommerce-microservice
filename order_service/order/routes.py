bbda3af6d639dbb86422eaee90d65cc0a629b281
Ummer Farooq <farooq.tfora@gmail.com>
2025-01-02 14:15:32 +0530

    Add token verification and improve order creation error handling

--- order_service/order/routes.py
+++ order_service/order/routes.py
@@ -1,21 +1,33 @@
 from core.db import get_session
-from fastapi import APIRouter, Depends
-from messaging.services import publish_product_stock_reduction, validate_product_stock
+from core.token import verify_token
+from fastapi import APIRouter, Depends, HTTPException, Request
+from messaging.services import publish_product_stock_reduction
 from order import crud
+from order.services import validate_product_stock
 
 from .schemas import OrderRequest
 
-router = APIRouter(prefix="/orders", tags=["Orders"])
+router = APIRouter(
+    prefix="/orders", tags=["Orders"], dependencies=[Depends(verify_token)]
+)
 
 
 @router.post("/create")
-def create_order(request: OrderRequest, session=Depends(get_session)):
-    current_user = 1
-    is_stock_available = validate_product_stock(request.dict())
-    if not is_stock_available["in_stock"]:
-        return {"message": is_stock_available["message"]}
+def create_order(
+    request: Request, request_data: OrderRequest, session=Depends(get_session)
+):
+    current_user = request.state.user
 
-    order = crud.create_order(current_user, session, request)
+    try:
+        is_stock_available = validate_product_stock(
+            request_data.dict(), request.state.bearer_token
+        )
+        if not is_stock_available["in_stock"]:
+            return {"message": is_stock_available["message"]}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    order = crud.create_order(current_user, session, request_data)
     publish_product_stock_reduction(order)
     return {"message": "Order placed successfully"}
 
