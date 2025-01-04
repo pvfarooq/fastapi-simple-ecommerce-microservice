a1f510de83fac847cbf1c20d94311aec122aba63
Ummer Farooq <farooq.tfora@gmail.com>
2025-01-04 17:11:55 +0530

    cleanup: linting and formatting

--- services/order/alembic/env.py
+++ services/order/alembic/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config, pool
+
 from alembic import context
 from core.db import Base, get_db_url
 from order.models import Order
-from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
